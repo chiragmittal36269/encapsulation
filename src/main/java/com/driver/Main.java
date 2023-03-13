0e5dd663b1b715f49dd342a461e9416f5a478e07
Chirag Mittal <118048068+chiragmittal36269@users.noreply.github.com>
2023-03-13 23:29:33 +0530

    Add files via upload

---
+++ src/main/java/com/driver/Main.java
@@ -0,0 +1,13 @@
+package com.driver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        RWOnly r = new RWOnly();
+
+        r.setName("Chirag Mittal");
+        r.getName();
+
+    }
+  
+}
